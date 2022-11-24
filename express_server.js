3d2e232ee01a4bfcdde7934d5ad8f805ebeba140
Marcela Ang <marcela.ang@gmail.com>
2022-11-24 11:55:57 -0500

    URL Shortening Part 1

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const PORT = 8080; //default port 8080
 
 app.set('view engine', 'ejs');
 
+const generateRandomString = function(){
+  return Math.random().toString(36).substr(2, 6)
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -36,17 +40,19 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//POST Route to Receive the Form Submission:
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
